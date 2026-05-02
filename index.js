72368def7c5aeb851acf1a5e657c49a9995b4004
0050505ishan <isan91798@gmail.com>
2026-05-02 08:00:17 +0545

    user model

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth.route");
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 
+//Routes
+app.use("/api/auth", authRoutes);
+
+//Connect to MongoDB
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
