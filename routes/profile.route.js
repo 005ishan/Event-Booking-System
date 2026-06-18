c99e22b3b357ba25489218d671cbda631d2d63d1
0050505ishan <isan91798@gmail.com>
2026-06-18 15:54:01 +0545

    updated

---
+++ routes/profile.route.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const { protect } = require("../middlewares/auth.middleware");
+const { getProfile, updateProfile, changePassword } = require("../controllers/profile.controller");
+
+router.get("/", protect, getProfile);
+router.put("/", protect, updateProfile);
+router.put("/password", protect, changePassword);
+
+module.exports = router;
